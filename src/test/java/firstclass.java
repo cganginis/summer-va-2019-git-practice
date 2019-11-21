d85e17ba8657da92cab30166ff6e6934c122fbba
christopher ganginis <cganginis@gmail.com>
2019-11-21 16:57:12 -0500

    thrid commit

--- src/test/java/firstclass.java
+++ src/test/java/firstclass.java
@@ -4,5 +4,6 @@ public class firstclass {
         System.out.println("hi world");
         System.out.println("im working dickhead");
         System.out.println("get off my ass bitch!" );
+        System.out.println("fuck this shit");
     }
 }
