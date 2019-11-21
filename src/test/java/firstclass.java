6667842f9f6824aa59b3ac41970cfcc1102db17b
cganginis <55109518+cganginis@users.noreply.github.com>
2019-11-21 16:38:25 -0500

    added a little something

--- src/test/java/firstclass.java
+++ src/test/java/firstclass.java
@@ -3,6 +3,6 @@ public class firstclass {
     public static void main(String[] args) {
         System.out.println("hi world");
         System.out.println("im working bitch");
-        System.out.println("get off my ass!" );
+        System.out.println("get off my ass bitch!" );
     }
 }
