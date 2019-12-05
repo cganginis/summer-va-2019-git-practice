6419ff58ab05dcfbbeca7d7a4fea73e82016b8cf
cganginis <55109518+cganginis@users.noreply.github.com>
2019-12-05 15:24:01 -0500

    Update secondclass.java

--- src/test/java/secondclass.java
+++ src/test/java/secondclass.java
@@ -3,5 +3,7 @@ public class secondclass {
     public static void main(String[] args) {
         String myname = "chris";
         System.out.println(myname);
+        System.out.println("chris");
+   
     }
 }
